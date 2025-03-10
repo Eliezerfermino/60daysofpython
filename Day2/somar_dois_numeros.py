76a82d59ccd01994631dd6847bbd10274c06a19f
Eliezer Fermino <eliezer.fermino.santos@gmail.com>
2025-03-10 13:24:55 -0300

    Adicionando o dia 2 de programaçao em python

---
+++ Day2/somar_dois_numeros.py
@@ -0,0 +1,5 @@
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+soma = int(num1) + int(num2)
+
+print(f"Essa é a soma de dois numeros em python: {soma}")
---
+++ Day2/somar_dois_numeros2.py
@@ -0,0 +1,17 @@
+def soma(num1, num2):
+    somar_dois_numeros = num1 + num2
+    print(somar_dois_numeros)
+    return somar_dois_numeros
+
+soma(1, 5)
+
+print("-" * 50)
+
+def somar_dois_num():
+    num1 = int(input("Digite um numero: "))
+    num2 = int(input("Digite outro numero: "))
+    somar_dois_num = num1 + num2
+    print(somar_dois_num)
+    return somar_dois_num
+
+somar_dois_num()
